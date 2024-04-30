87b495a9fdb19bad54c46bb259e182ab539e7047
Ameya Godse <ameyagodse@Ameyas-MBP.lan>
2024-04-30 11:28:53 +0100

    Added code for Cartpage as per changes requested for PR

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,6 +1,15 @@
 package com.qa.opencart.pages;
 
+import com.qa.opencart.logger.Log;
+import org.openqa.selenium.By;
+
 public class CartPage {
     int i =10;
     int y =100;
+
+    private By name = By.id("name");
+
+    public void getName(){
+        Log.info("Name is Wrathfool");
+    }
 }
